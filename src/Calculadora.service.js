d89f2627ec1289a4d84738ffc03e7d2696a603ad
alexsandro_ferrao <alexsandro_ferrao@sicredi.com.br>
2021-09-18 10:45:03 -0300

    Ajustando registry

---
+++ src/Calculadora.service.js
@@ -0,0 +1,33 @@
+function CalculadoraService() {
+  const SOMA = "+";
+  const SUBTRACAO = "-";
+  const MULTIPLICACAO = "*";
+  const DIVISAO = "/";
+
+  function calcular(primeiroNumero, segundoNumero, operacao) {
+    let resultado;
+
+    switch (operacao) {
+      case SOMA:
+        resultado = primeiroNumero + segundoNumero;
+        break;
+      case SUBTRACAO:
+        resultado = primeiroNumero - segundoNumero;
+        break;
+      case MULTIPLICACAO:
+        resultado = primeiroNumero * segundoNumero;
+        break;
+      case DIVISAO:
+        resultado = Math.round(primeiroNumero / segundoNumero);
+        break;
+      default:
+        resultado = 0;
+        break;
+    }
+
+    return resultado;
+  }
+  return [calcular, SOMA, SUBTRACAO, MULTIPLICACAO, DIVISAO];
+}
+
+export default CalculadoraService;
